339589f93fea46c63793038d88c3e8403f5c09c9
Steven Birkner <stevebirk36@gmail.com>
2014-03-11 15:45:56 -0400

    MOD: made the calc function better

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,24 @@
 /*
  * GET home page.
  */
+var math = require("../public/javascripts/math.js");
 
 exports.index = function(req, res){
   res.render('index', { title: 'Calculator' });
 };
 
 exports.calc = function(req, res){
-	console.log(req.body);
-	res.render('answer', { title: 'Answer'});
+	var num1 = Number(req.body.num1);
+	var num2 = Number(req.body.num2);
+	var op = req.body.op;
+	if (op == '+') {
+		var ans = math.add(num1,num2);
+	} else if (op == '-') {
+		var ans = math.subtract(num1,num2);
+	} else if (op == '*') {
+		var ans = math.multiply(num1,num2);
+	} else {
+		var ans = math.divide(num1,num2);
+	}
+	res.render('answer', { title: 'Answer', answer: ans });
 };
