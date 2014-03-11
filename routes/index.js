097c9311270d3c731d0a074fc7dd310a168225d2
Steven Birkner <stevebirk36@gmail.com>
2014-03-11 15:24:35 -0400

    ADD: calc function and answer page

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,8 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Calculator' });
 };
+
+exports.calc = function(req, res){
+	console.log(req.body);
+	res.render('answer', { title: 'Answer'});
+};
