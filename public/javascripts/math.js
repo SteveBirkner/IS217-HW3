851193ae61cee1892bcd535f0dbaf010b4fc47d4
Steven Birkner <stevebirk36@gmail.com>
2014-03-11 15:32:59 -0400

    ADD: math.js functions

---
+++ public/javascripts/math.js
@@ -0,0 +1,17 @@
+exports = module.exports = {};
+
+exports.add = function(x,y){
+	return x + y;
+};
+
+exports.subtract = function(x,y){
+	return x - y;
+};
+
+exports.multiply = function(x,y){
+	return x * y;
+};
+
+exports.divide = function(x,y){
+	return x / y;
+};
